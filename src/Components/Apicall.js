43ba7be4b9ecbffa6e7ec1a36a183b42c315fa81
ellisrugazoora <ellisrgrz@gmail.com>
2024-11-26 15:58:55 +0300

    Variable prompt

--- src/Components/Apicall.js
+++ src/Components/Apicall.js
@@ -1,16 +1,17 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 
 function Apicall(){
-
+    const [prompt, SetPrompt] = useState("How old is the sun?")
     const backendUrl = "https://nodebackend-smmy.onrender.com/api/openai";
     const sendRequest = async () => {
+        console.log(prompt)
         try {
             const response = await fetch(backendUrl, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify({ prompt : "How old is the sun?" }),
+                body: JSON.stringify({ prompt : prompt }),
             })
             const data = await response.json();
             console.log(data.message.content);
@@ -21,6 +22,7 @@ function Apicall(){
     return (
         <div>
             <p>API call</p>
+            Prompt: <input type="text" placeholder="Enter prompt" onChange={(e)=>{SetPrompt(e.target.value); console.log(prompt)}}/>
             <button onClick={sendRequest}>Send request</button>
             
         </div>
