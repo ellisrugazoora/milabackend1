11a3edc836b386f264a0fd39425d111555fd8e61
ellisrugazoora <ellisrgrz@gmail.com>
2024-11-29 17:59:46 +0300

    Automatically receive webhooks

--- src/Components/Webhook.js
+++ src/Components/Webhook.js
@@ -1,8 +1,24 @@
-import { useState } from "react";
-import { io } from "socket.io-client";
+import { useEffect, useState } from "react";
+// import { io } from "socket.io-client";
 
 function Webhook(){
     const [webhooks, setWebhooks] = useState(0);
+    const [webhookData, setWebhookData] = useState(null);
+    useEffect(() => {
+        const eventSource = new EventSource("https://nodebackend-smmy.onrender.com/events");
+    
+        // Listen for messages from the server
+        eventSource.onmessage = (event) => {
+          const data = JSON.parse(event.data);
+          console.log("Received webhook data:", data);
+          setWebhookData(data); // Update the state with the new webhook data
+        };
+    
+        // Close the connection when the component is unmounted
+        return () => {
+          eventSource.close();
+        };
+      }, []);
     const fetchwebhook = async () => {
         const response = await fetch('https://nodebackend-smmy.onrender.com/webhook');
         const data = await response.json();
@@ -10,10 +26,14 @@ function Webhook(){
         setWebhooks(data)
         //alert(JSON.stringify(data, null, 2)); // Show the data in an alert box
     }
+    function print(){
+        console.log(webhooks)
+    }
     return (
         <div>
             <button onClick={fetchwebhook}>Fetch Webhooks</button>
-            {/* {JSON.stringify(webhooks, null, 2)} */}
+            <p>{JSON.stringify(webhookData)}</p>
+            <button onClick={print}>print</button>
         </div>
     )
 }
